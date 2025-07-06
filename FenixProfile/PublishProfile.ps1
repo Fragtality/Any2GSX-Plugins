2ccaba80609d39cee704334900075f7abfdae2b7
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-07-06 23:55:56 +0200

    Updated/Corrected Profile Versions

--- FenixProfile/PublishProfile.ps1
+++ FenixProfile/PublishProfile.ps1
@@ -1,9 +1,10 @@
 $currentDir = $pwd.Path
 $pathProfileDeck = Resolve-Path "FNX.PilotsDeck.json"
 $pathProfileNative = Resolve-Path "FNX.Native.json"
+$versionProfile = "0.1.1"
 
 cd ..\dist
-.\AddProfileToRepo.ps1 $pathProfileDeck "Fenix - PilotsDeck only" "Fenix A320 (all Variants)" "Profile enabling only PilotsDeck Support (to be used together with Fenix2GSX)" "Fragtality" "0.1.0" "0.1.0"
-.\AddProfileToRepo.ps1 $pathProfileNative "Fenix - Native" "Fenix A320 (all Variants)" "Profile enhancing Fenix' native GSX Integration with Automation and Volume Control" "Fragtality" "0.1.0" "0.1.0"
+.\AddProfileToRepo.ps1 $pathProfileDeck "Fenix - PilotsDeck only" "Fenix A320 (all Variants)" "Profile enabling only PilotsDeck Support (to be used together with Fenix2GSX)" "Fragtality" "$versionProfile" "0.1.1"
+.\AddProfileToRepo.ps1 $pathProfileNative "Fenix - Native" "Fenix A320 (all Variants)" "Profile enhancing Fenix' native GSX Integration with Automation and Volume Control" "Fragtality" "$versionProfile" "0.1.1"
 
 cd $currentDir
