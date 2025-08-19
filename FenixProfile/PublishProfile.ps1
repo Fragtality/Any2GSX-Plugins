e3489da5e87762ed291d5d7d1ef6b905bf4d38f1
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-08-19 15:32:23 +0200

    Fenix Profiles: disabled SimBrief Refresh in GSX on Departure and Turnaround

--- FenixProfile/PublishProfile.ps1
+++ FenixProfile/PublishProfile.ps1
@@ -4,7 +4,7 @@ $pathProfileNative = Resolve-Path "FNX.Native.json"
 $versionProfile = "0.1.3"
 
 cd ..\dist
-.\AddProfileToRepo.ps1 $pathProfileDeck "Fenix - PilotsDeck only" "Fenix A320 (all Variants)" "Profile enabling only PilotsDeck Support (to be used together with Fenix2GSX)" "Fragtality" "$versionProfile" "0.1.1"
-.\AddProfileToRepo.ps1 $pathProfileNative "Fenix - Native" "Fenix A320 (all Variants)" "Profile enhancing Fenix' native GSX Integration with Automation and Volume Control" "Fragtality" "$versionProfile" "0.1.1"
+.\AddProfileToRepo.ps1 $pathProfileDeck "Fenix - PilotsDeck only" "Fenix A320 (all Variants)" "Profile enabling only PilotsDeck Support (to be used together with Fenix2GSX)" "Fragtality" "$versionProfile" "0.1.9"
+.\AddProfileToRepo.ps1 $pathProfileNative "Fenix - Native" "Fenix A320 (all Variants)" "Profile enhancing Fenix' native GSX Integration with Automation and Volume Control" "Fragtality" "$versionProfile" "0.1.9"
 
 cd $currentDir
