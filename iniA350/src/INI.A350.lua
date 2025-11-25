45a09d457c8373230a6367006bf206591aa3bae9
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-11-25 19:45:36 +0100

    PMDG.B777: Update to PluginInterface for new ServiceConstraint / INI.A350: Removed Payload Synch - defunct by iniBuilds

--- iniA350/src/INI.A350.lua
+++ iniA350/src/INI.A350.lua
@@ -174,25 +174,6 @@ function SetFuelOnBoardKg(fuelKg)
     WriteVar("L:INI_FUEL_REQ", fuelKg)
 end
 
-local payloadPaxKg = 0
-local payloadCargoKg = 0
-
-function SetPayloadStations()
-    local payloadTotalKg = payloadPaxKg + payloadCargoKg
-    WriteVar("L:INI_PAX_BUSSINESS", payloadTotalKg * 0.0492)
-    WriteVar("L:INI_PAX_ECONOMY_1", payloadTotalKg * 0.1300)
-    WriteVar("L:INI_PAX_ECONOMY_2", payloadTotalKg * 0.1585)
-    WriteVar("L:INI_CARGO_FWD_BAGGAGE_KG", payloadTotalKg * 0.3356)
-    WriteVar("L:INI_CARGO_AFT_BAGGAGE_KG", payloadTotalKg * 0.275)
-    WriteVar("L:INI_CARGO_BULK_BAGGAGE_KG", payloadTotalKg * 0.0517)
-end
-
-function SetPayloadEmpty()
-    payloadPaxKg = 0
-    payloadCargoKg = 0
-    SetPayloadStations()
-end
-
 local refuelWasOverridden = false
 
 function RefuelActive()
@@ -228,102 +209,6 @@ function RefuelStop(fuelTargetKg, setTarget)
     end
 end
 
-local boardWasOverriden = false
-
-function BoardActive(paxTarget, cargoTargetKg)
-    if not GetSetting("INI.350.Option.Override.Board") then
-        Log("Using native Board Payload Synch")
-        return
-    else
-        Log("Using Any2GSX Payload Synch on Boarding")
-    end
-
-    if boardWasOverriden then
-        return
-    end
-    boardWasOverriden = true
-
-    gsxController.GetService(4).StateOverride = 5
-    WriteVar("L:FSDT_GSX_BOARDING_STATE", 2)
-
-    payloadPaxKg = 0
-    payloadCargoKg = 0
-    SetPayloadStations()
-end
-
-function BoardChangePax(paxOnBoard, weightPerPaxKg)
-    if not GetSetting("INI.350.Option.Override.Board") then
-        return
-    end
-
-    payloadPaxKg = paxOnBoard * weightPerPaxKg
-    SetPayloadStations()
-end
-
-function BoardChangeCargo(progressLoad, cargoOnBoardKg)
-    if not GetSetting("INI.350.Option.Override.Board") then
-        return
-    end
-
-    payloadCargoKg = cargoOnBoardKg
-    SetPayloadStations()
-end
-
-function BoardCompleted(paxTarget, weightPerPaxKg, cargoTargetKg)
-    if not GetSetting("INI.350.Option.Override.Board") then
-        return
-    end
-
-    payloadPaxKg = paxTarget * weightPerPaxKg
-    payloadCargoKg = cargoTargetKg
-    SetPayloadStations()
-end
-
-function DeboardChangePax(paxOnBoard, gsxTotal, weightPerPaxKg)
-    if not GetSetting("INI.350.Option.Override.Deboard") then
-        return
-    end
-    payloadPaxKg = paxOnBoard * weightPerPaxKg
-    SetPayloadStations()
-end
-
-function DeboardChangeCargo(progressUnload, cargoOnBoardKg)
-    if not GetSetting("INI.350.Option.Override.Deboard") then
-        return
-    end
-    payloadCargoKg = cargoOnBoardKg
-    SetPayloadStations()
-end
-
-local deboardWasOverriden = false
-
-function DeboardActive()
-    if not GetSetting("INI.350.Option.Override.Deboard") then
-        Log("Using native Deboard Payload Synch")
-        return
-    else
-        Log("Using Any2GSX Payload Synch on Deboarding")
-    end
-
-    if deboardWasOverriden then
-        return
-    end
-    deboardWasOverriden = true
-
-    gsxController.GetService(7).StateOverride = 5
-    WriteVar("L:FSDT_GSX_DEBOARDING_STATE", 2)
-end
-
-function DeboardCompleted()
-    if not GetSetting("INI.350.Option.Override.Deboard") then
-        return
-    end
-
-    payloadPaxKg = 0
-    payloadCargoKg = 0
-    SetPayloadStations()
-end
-
 function SetDoorArmState(armed)
     local isArmed = ReadVar("L:INI_DOOR0_ARMED") > 0
     if (isArmed and not armed) or (not isArmed and armed) then
@@ -334,7 +219,6 @@ end
 
 function OnAutomationStateChange(state)
     if state == 1 or state == 2 then --prep/departure
-        deboardWasOverriden = false
         SetDoorArmState(false)
         if GetSetting("INI.350.Option.Covers.RemoveStartup") then
             WriteVar("L:INI_COVERS_ENABLED", 0)
@@ -342,14 +226,10 @@ function OnAutomationStateChange(state)
     elseif state == 4 then --taxiout
         SetDoorArmState(true)
         refuelWasOverridden = false
-        boardWasOverriden = false
     elseif state == 5 then --flight
         refuelWasOverridden = false
-        boardWasOverriden = false
     elseif state == 7 then --arrival
         SetDoorArmState(false)
-    elseif state == 8 then --turn
-        deboardWasOverriden = false
     end
 end
 
