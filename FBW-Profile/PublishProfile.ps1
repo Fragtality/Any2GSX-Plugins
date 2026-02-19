11120b84ed8bf391b0996d88573e16a7125d8666
Fragtality <78614902+Fragtality@users.noreply.github.com>
2026-02-19 18:39:59 +0100

    B777: Changes to PluginInterface / FBW Profiles: Set GSX GPU Usage to NoJetway

--- FBW-Profile/PublishProfile.ps1
+++ FBW-Profile/PublishProfile.ps1
@@ -1,7 +1,7 @@
 $currentDir = $pwd.Path
 $pathProfile320 = Resolve-Path "FBW.A320-Profile.json"
 $pathProfile380 = Resolve-Path "FBW.A380-Profile.json"
-$versionProfile = "0.1.1"
+$versionProfile = "0.1.2"
 
 cd ..\dist
 .\AddProfileToRepo.ps1 $pathProfile320 "FlyByWire A320" "FBW A320 (stable/dev)" "Profile for Automation and Volume-Control on the FBW A320.\r\nImport the Flightplan in the MCDU to trigger Departure Services.\r\nDon't forget to power on the EFB first!\r\nThe SmartButton is mapped to the MECH Call Button on the Overhead." "Fragtality" "$versionProfile" "0.1.1"
